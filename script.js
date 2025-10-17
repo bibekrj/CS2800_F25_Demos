0ac7d24f7843158002a761dcb081a294416e4f51
bibekrj <29625141+bibekrj@users.noreply.github.com>
2025-10-16 21:19:43 -0400

    Oct 16, 2025 - Class on JavaScript

---
+++ script.js
@@ -0,0 +1,9 @@
+ function addTwo(a,b){
+      return a+b;
+    }
+function total(userName){
+      let val1 = Number(prompt("What is your first value"));
+      let val2 = Number(prompt("What is your second value?"));
+      alert("hello " + userName + " the total is "+ addTwo(val1, val2));
+    }
+
