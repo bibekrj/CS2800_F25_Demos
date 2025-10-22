1c65b498280aae4903f7f54ac9f3c4df200a5754
bibekrj <29625141+bibekrj@users.noreply.github.com>
2025-10-21 21:57:35 -0400

    Oct 21 2025 - Calling functions from HTML - Calling function using onClick - Calling function via form - Interacting w/ data

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
  function addTwo(a,b){
       return a+b;
     }
+
 function total(userName){
       let val1 = Number(prompt("What is your first value"));
       let val2 = Number(prompt("What is your second value?"));
       alert("hello " + userName + " the total is "+ addTwo(val1, val2));
     }
 
+
+function printName(){
+  let first_name =document.getElementById("firstName").value;
+  let last_name = document.getElementById("lastName").value;
+  // console.log("Test message");
+    alert("Hello " + first_name + " " + last_name);
+}
