6123f89446962d1039b0648c9ba1c2d3d2c9c186
bibekrj <29625141+bibekrj@users.noreply.github.com>
2025-10-23 21:24:16 -0400

    10/23/2025 JavaScript Class - Topics Covered Usage of onClick and onSubmit Usage of DateObject in JS Usage of document.forms

--- script.js
+++ script.js
@@ -10,8 +10,32 @@ function total(userName){
 
 
 function printName(){
-  let first_name =document.getElementById("firstName").value;
-  let last_name = document.getElementById("lastName").value;
-  // console.log("Test message");
-    alert("Hello " + first_name + " " + last_name);
+  // let first_name =document.getElementById("firstName").value;
+  // let last_name = document.getElementById("lastName").value;
+  // // console.log("Test message");
+  //   alert("Hello " + first_name + " " + last_name);
+  let firstName = document.forms["contactForm"]["firstName"].value;
+  let lastName = document.forms["contactForm"]["lastName"].value;
+  if( lastName == ""){
+    alert("Hey You for to enter the last name");
+  }
+  else{
+    alert("Hello " + firstName + " " + lastName);
+  }
+
+}
+
+function timeStamp(){
+   const today_date = new Date();
+    // console.log(today_date);
+      const month = Number(today_date.getMonth())+1;
+    let message = "Your order was placed on "+today_date.getFullYear()+"-"+month+"-"+today_date.getDate();
+    document.getElementById("finalMessage").innerHTML = message;
+}
+
+function changeColor(change){
+  // let change = true;
+  let heading = document.getElementById("colorChangeH1");
+  heading.style.color="red";
+
 }
